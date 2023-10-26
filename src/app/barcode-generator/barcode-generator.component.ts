0be2fbd0d75b9767387d5c39185176562bef836c
Dean <dean.b.98@hotmail.com>
2023-10-26 23:00:27 +0100

    Fix vercel issue

--- src/app/barcode-generator/barcode-generator.component.ts
+++ src/app/barcode-generator/barcode-generator.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { BarcodeService } from './barcode.service';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-barcode-generator',
